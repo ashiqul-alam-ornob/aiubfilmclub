6bd52a66dbd71049ba31eac3053906e0284b3ca6
Md. Ali Arman Frahim <aafrahim@gmail.com>
2019-11-01 10:48:03 +0600

    home controller and view is reverted to previous state

--- controllers/home.js
+++ controllers/home.js
@@ -1,31 +1,17 @@
 var express = require('express');
-var userModel = require('./../models/user-model');
+var db = require('./../models/db.js');
 var router = express.Router();
 
 router.get('/', function(req, res){
-	if(req.cookies['userid'] == null || req.session.userid == null){
-		res.render('home/index', {user: null});
-	}else{
-		var uid;
-		if(req.cookies['userid']!=null)
-		{
-			uid = req.cookies['userid'];
-		}
-		else
-		{
-			uid = req.session.userid;
-		}
 
-		userModel.getById(uid, function(result){
-			if(req.session.userid == null)
-			{
-				req.session.userid = result[0].userid;
-				req.session.usertype = result[0].usertype;
-				req.session.designation = result[0].designation; 
+		var sql = "select * from userinfo";
+		db.getResults(sql, function(results){
+			if(req.cookies['userid'] != null){
+				res.render('home/index', {user: results});
+			}else{
+				res.redirect('/login');
 			}
-			res.render('home/index', {user: result[0]});
 		});
-	}
 });
 
 
