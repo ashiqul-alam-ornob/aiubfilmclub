fb165b0c8bfabbee641f4a12bdf77cd4454f222b
Md. Ali Arman Frahim <aafrahim@gmail.com>
2019-11-01 22:59:34 +0600

    Modify post controller for create post

--- controllers/post.js
+++ controllers/post.js
@@ -1,17 +1,48 @@
 var express = require('express');
-var db = require('./../models/db.js');
+var postModel = require('./../models/post-model');
 var router = express.Router();
 
-router.get('/', function(req, res){
+router.get('*', function(req, res, next){
 
-    var sql = "select * from postinfo";
-    db.getResults(sql, function(results){
-        if(req.cookies['userid'] != null){
-            res.render('home/index', {user: results});
-        }else{
-            res.redirect('/login');
-        }
-    });
+	if(req.cookies['userid'] != null || req.session.userid != null){
+		next();
+	}else{
+		res.redirect('/home');
+	}
+});
+
+router.get('/createpost', function(req, res){
+	var user = {
+		designation: req.session.designation
+	};
+	res.render('post/createpost', {user: user});
+});
+
+router.post('/createpost', function(req, res){
+	var fp; 
+	if(req.body.filepath == "" || req.body.filepath == undefined || req.body.filepath == null)
+	{
+		fp = null;
+	}
+	else{
+		fp = req.body.filepath;
+	}
+
+	var post = {
+		userid: req.session.userid,
+		postdescription: req.body.postdescription,
+		postdate: new Date().toISOString().slice(0,10),
+		postapprovalstatus: null,
+		filepath: fp
+	};
+
+	postModel.insert(post, function(status){
+		if(status){
+			res.redirect('/home');
+		}else{
+			res.redirect('/post/createpost');
+		}
+	});
 });
 
 module.exports = router;
